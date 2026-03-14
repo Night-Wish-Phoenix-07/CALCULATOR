3b0568a3236d3349081d4faf859c1a568478427b
Sujitha <mssujitha10307@gmail.com>
2026-03-14 10:34:52 +0530

    CALCULATION  support for multiple arithmetic operations
    Implement basic arithmetic operations in pro function

---
+++ calculation.py
@@ -0,0 +1,20 @@
+#PRO
+def pro(N):
+  if N == "ADD":
+    a=A+B
+    return a
+  elif N == "SUB":
+    a=A+B
+    return a
+  elif N == "MUL":
+    a=A*B
+    return a
+  elif N == "DIV":
+    a=A/B
+    return a
+  elif N == "FLO":
+    a=A//B
+    return a
+  elif N == "POW":
+    a=A**B
+    return a
